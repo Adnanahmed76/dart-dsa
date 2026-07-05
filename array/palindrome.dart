cc2ff4dd98256dedf2cf5da370442e1f6f478481
adnan ahmed <adnanahmed504423@gmail.com>
2026-07-05 18:44:52 +0530

    largest number array

---
+++ array/palindrome.dart
@@ -0,0 +1,26 @@
+void main() {
+  // String s = "madam";
+  // String isresult = ispalindrome(s);
+  // print("$isresult");
+}
+
+// bool isPalindrome(String s){
+
+// }
+
+// String ispalindrome(String s) {
+//   int right = s.length - 1;
+//   int length = s.length;
+
+//   print("length: $right");
+//   for (int i = 0; i < length; i++) {
+//     //0 1 2 3 4
+//     for (int j = right; j >= 0; j--) {
+//       if (s[i] == s[j]) {
+//         return "palindrome";
+//       }
+//     }
+//   }
+//   return "not palindrome";
+// }
+
